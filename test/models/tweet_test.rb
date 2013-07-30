f227589364edf5cb6e746dcccce942195abd45ca
Nick Small <nick@shopify.com>
2013-07-29 21:46:11 -0400

    Add user associations to tweet fixtures.

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -4,4 +4,9 @@ class TweetTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "tweet can get user's id" do
+  	tweet = tweets(:one)
+  	assert_equal tweet.user.id, users(:one).id
+  end
 end
